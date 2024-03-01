5cb6d52b242851da1ef8f08eaeead9658bedb900
2blexn <300amfkall@gmail.com>
2024-03-01 11:43:14 +0200

    ghf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { functions } from 'lodash';
+import { count } from 'rxjs';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  ///4.	Підрахунок кількості слів у реченні.
+
+  let value: string;
+  value = "Hello world";
+  function getSubString(){
+  
+  }
+  alert(value)
